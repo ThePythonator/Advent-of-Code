fdaa23cd22ede554f13bbe2c93d9d5d40d8abc03
Joseph Hearn <49040244+ThePythonator@users.noreply.github.com>
2023-12-15 17:47:40 +0000

    Improved template file

--- template.py
+++ template.py
@@ -1,11 +1,36 @@
 import os
+from time import time
 SCRIPT_PATH = os.path.abspath(os.path.dirname(__file__))
 
+############################################################
+# Main solution code here
+
+def solve(lines):
+    result = 0
+    for i, line in enumerate(lines):
+        result += i
+    
+    return result
+
+
+############################################################
+# Boilerplate
+
+with open(os.path.join(SCRIPT_PATH, "sample.txt")) as f:
+    sample = [line.strip() for line in f.readlines()]
+
 with open(os.path.join(SCRIPT_PATH, "input.txt")) as f:
-    lines = [line.strip() for line in f.readlines()]
+    puzzle = [line.strip() for line in f.readlines()]
 
-result = 0
-for i, line in enumerate(lines):
-    pass
+SAMPLE_START = time()
+sample_result = solve(sample)
+print("Sample:")
+print(f"Time: {time() - SAMPLE_START}")
+print(f"Result: {sample_result}")
+print()
 
-print(f"Result: {result}")
+PUZZLE_START = time()
+puzzle_result = solve(puzzle)
+print("Puzzle:")
+print(f"Time: {time() - PUZZLE_START}")
+print(f"Result: {puzzle_result}")
