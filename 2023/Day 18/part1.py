c86b59f256c2dd3dcbf60722c3b93283cc51e9e6
Joseph Hearn <49040244+ThePythonator@users.noreply.github.com>
2023-12-18 11:36:21 +0000

    Used D18P2 solution to speed up D18P1

--- 2023/Day 18/part1.py
+++ 2023/Day 18/part1.py
@@ -6,61 +6,96 @@ SCRIPT_PATH = os.path.abspath(os.path.dirname(__file__))
 # Main solution code here
 
 def solve(lines):
-    grid = [["#"]]
-    position = [0,0]
+    x = y = 0
+    vertices = []
     for line in lines:
         direction, distance, colour = line.split()
         distance = int(distance)
-        while distance > 0:
-            if direction == "R":
-                position[0] += 1
-            elif direction == "D":
-                position[1] += 1
-            elif direction == "L":
-                position[0] -= 1
-            elif direction == "U":
-                position[1] -= 1
-            if position[0] < 0:
-                position[0] += 1
-                for i in range(len(grid)):
-                    grid[i].insert(0, ".")
-            elif position[0] >= len(grid[0]):
-                for i in range(len(grid)):
-                    grid[i].append(".")
-            if position[1] < 0:
-                position[1] += 1
-                grid.insert(0, ["."]*len(grid[0]))
-            elif position[1] >= len(grid):
-                grid.append(["."]*len(grid[0]))
-            grid[position[1]][position[0]] = "#"
-            distance -= 1
-    
+        if direction == "R":
+            x += distance
+        elif direction == "D":
+            y += distance
+        elif direction == "L":
+            x -= distance
+        elif direction == "U":
+            y -= distance
+        vertices.append((x,y))
+
+    ys = sorted(vertices, key=lambda x:(x[1],x[0]))
+    rows = []
+    while len(ys) > 0:
+        # Remove all ys in the same layer
+        vs = [ys.pop(0)]
+        while len(ys) > 0 and ys[0][1] == vs[0][1]:
+            vs.append(ys.pop(0))
+        rows.append(vs)
+
     result = 0
-    for y, l in enumerate(grid):
-        in_trench = False
-        last_c = "."
-        last_in_trench = False
-        entry_corner = exit_corner = ""
-        inside = False
-        trench_length = 0
-        for x, c in enumerate(l):
-            if c == "#":
-                if last_c == ".":
-                    in_trench = True
-                    entry_corner = "U" if y > 0 and grid[y-1][x] == "#" else "D"
-            elif c == ".":
-                if last_c == "#":
-                    in_trench = False
-                    exit_corner = "D" if y < len(grid) - 1 and grid[y+1][x-1] == "#" else "U"
-            if in_trench:
-                trench_length += 1
-            if last_in_trench and not in_trench and (trench_length == 1 or entry_corner != exit_corner):
-                inside = not inside
-                trench_length = 0
-            if inside or c == "#":
-                result += 1
-            last_c = c
-            last_in_trench = in_trench
+    lines = [] # x coords of existing lines
+    prev_y = 0
+    for i in range(len(rows)):
+        row = rows[i]
+        lines.sort()
+        y = row[0][1]
+
+        for j in range(0, len(lines), 2):
+            # Get extruded shape width and height
+            result += (lines[j+1] - lines[j] + 1) * (y - prev_y - 1)
+
+        directions = {x: "L" for x in lines}
+        for x, y in row:
+            terminated = False
+            for p_x in lines[::-1]:
+                if x == p_x:
+                    # The line terminates here
+                    lines.remove(p_x)
+                    terminated = True
+                    directions[p_x] = "U"
+                    break
+
+            if not terminated:
+                # The line starts here
+                lines.append(x)
+                directions[x] = "D"
+
+        # Get width of current row
+        inside = 0
+        last_x = 0
+        last_d = ""
+        w = 0
+        for x, d in sorted(directions.items()):
+            if inside == 1:
+                if d == "L":
+                    inside = 0
+                    w += x - last_x + 1
+                else:
+                    inside = 1.5
+
+            elif inside == 0.5:
+                assert d != "L"
+                if d != last_d:
+                    inside = 1
+                else:
+                    inside = 0
+                    w += x - last_x + 1
+
+            elif inside == 1.5:
+                assert d != "L"
+                if d != last_d:
+                    inside = 0
+                    w += x - last_x + 1
+                else:
+                    inside = 1
+
+            else:
+                inside = 1 if d == "L" else 0.5
+                last_x = x
+
+            last_d = d
+
+        result += w
+        prev_y = y
+        
 
     return result
 
