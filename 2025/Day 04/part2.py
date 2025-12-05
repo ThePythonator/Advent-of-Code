cb1425029c57aabc1d511b24d03e7ecf9647ed55
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2025-12-05 22:08:00 +0000

    Day 4

---
+++ 2025/Day 04/part2.py
@@ -0,0 +1,76 @@
+import os
+from time import time
+SCRIPT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+############################################################
+# Main solution code here
+
+def solve(lines):
+    result = 0
+    grid = []
+    for i, line in enumerate(lines):
+        grid.append(line)
+
+    progress = True
+    while progress:
+        progress = False
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                adj = 0
+                for dx in [-1,0,1]:
+                    for dy in [-1,0,1]:
+                        if dx == 0 and dy == 0:
+                            continue
+                        elif (x == 0 and dx == -1) or (x == len(grid[0])-1 and dx == 1):
+                            continue
+                        elif (y == 0 and dy == -1) or (y == len(grid)-1 and dy == 1):
+                            continue
+                            
+                        px, py = x+dx, y+dy
+                        if grid[py][px] == "@":
+                            adj += 1
+                if adj < 4 and grid[y][x] == "@":
+                    grid[y] = grid[y][:x] + "." + grid[y][x+1:]
+                    progress = True
+                    result += 1
+    
+    return result
+
+
+############################################################
+# Boilerplate
+
+with open(os.path.join(SCRIPT_PATH, "sample.txt")) as f:
+    sample = [line.strip() for line in f.readlines()]
+
+with open(os.path.join(SCRIPT_PATH, "input.txt")) as f:
+    puzzle = [line.strip() for line in f.readlines()]
+
+UNITS = ["s", "ms", "μs"]
+
+SAMPLE_START = time()
+sample_result = solve(sample)
+time_taken = time() - SAMPLE_START
+
+unit_idx = 0
+while time_taken < 1 and unit_idx < len(UNITS) - 1:
+    time_taken *= 1000
+    unit_idx += 1
+
+print("Sample:")
+print(f"Time: {time_taken:.2f}{UNITS[unit_idx]}")
+print(f"Result: {sample_result}")
+print()
+
+PUZZLE_START = time()
+puzzle_result = solve(puzzle)
+time_taken = time() - PUZZLE_START
+
+unit_idx = 0
+while time_taken < 1 and unit_idx < len(UNITS) - 1:
+    time_taken *= 1000
+    unit_idx += 1
+
+print("Puzzle:")
+print(f"Time: {time_taken:.2f}{UNITS[unit_idx]}")
+print(f"Result: {puzzle_result}")
